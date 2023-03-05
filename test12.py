fb0c1d9f011d11004dd643d97e9d7e421a8b6936
Juan Ponton Rodriguez <juanpontonrodri@gmail.com>
2023-03-05 19:17:22 +0100

    medias 3 equipos

--- test12.py
+++ test12.py
@@ -25,33 +25,16 @@ combinaciones_por_equipo = []
 for equipo in equipos.values():
     combinaciones = itertools.combinations(equipo, 2)
     combinaciones_por_equipo.append(list(combinaciones))
-# Bucle para encontrar la(s) combinación(es) con la mayor puntuación para el primer equipo
-max_puntuacion = 0
-mejor_combinacion_equipo1 = None
-mejores_combinaciones = []
-for combinacion_equipo1 in combinaciones_por_equipo[0]:
-    puntuaciones_equipo1 = [6, 5, 4, 3, 2, 1]
-    for combinacion_equipo2 in combinaciones_por_equipo[1]:
-        for combinacion_equipo3 in combinaciones_por_equipo[2]:
-            tiempos = sorted(list(combinacion_equipo1) + list(combinacion_equipo2) + list(combinacion_equipo3), key=lambda x: x[1])
-            puntuacion_equipo1 = sum([puntuaciones_equipo1[i] for i in range(len(tiempos)) if tiempos[i] in combinacion_equipo1])
-            if puntuacion_equipo1 > max_puntuacion:
-                max_puntuacion = puntuacion_equipo1
-                mejor_combinacion_equipo1 = combinacion_equipo1
-                mejores_combinaciones = [(combinacion_equipo1, combinacion_equipo2, combinacion_equipo3)]
-            elif puntuacion_equipo1 == max_puntuacion:
-                mejores_combinaciones.append((combinacion_equipo1, combinacion_equipo2, combinacion_equipo3))
+
 
 # Bucle para encontrar las 6 combinaciones con la mayor puntuación media para el primer equipo
 puntuaciones_medias = []
 for combinacion_equipo1 in combinaciones_por_equipo[0]:
-    if mejor_combinacion_equipo1 and combinacion_equipo1 == mejor_combinacion_equipo1:
-        continue
     puntuacion_total = 0
     for combinacion_equipo2 in combinaciones_por_equipo[1]:
         for combinacion_equipo3 in combinaciones_por_equipo[2]:
             tiempos = sorted(list(combinacion_equipo1) + list(combinacion_equipo2) + list(combinacion_equipo3), key=lambda x: x[1])
-            puntuaciones = [6, 5, 4, 3, 2, 1]
+            puntuaciones = [7, 5, 4, 3, 2, 1]
             puntuacion_equipo1 = sum([puntuaciones[i] for i in range(len(tiempos)) if tiempos[i] in combinacion_equipo1])
             puntuacion_total += puntuacion_equipo1
     puntuacion_media = puntuacion_total / (len(combinaciones_por_equipo[1]) * len(combinaciones_por_equipo[2]))
@@ -61,23 +44,14 @@ for combinacion_equipo1 in combinaciones_por_equipo[0]:
 puntuaciones_medias.sort(key=lambda x: x[1], reverse=True)
 
 # Seleccionar las 6 combinaciones con la puntuación media más alta
-mejores_combinaciones_medias = puntuaciones_medias[:6]
+mejores_combinaciones_medias = puntuaciones_medias[:10]
 
-# Escritura de la tabla de resultados para la primera parte del problema en el archivo de salida
-with open(args.archivo_salida, 'w',encoding='utf-8', newline='') as archivo:
-    writer = csv.writer(archivo)
-    writer.writerow(["Combinación", "Equipo 1", "Equipo 2", "Equipo 3", "Puntuación"])
-    for idx, mejor_combinacion in enumerate(mejores_combinaciones):
-        equipo1 = ", ".join([n[0] for n in mejor_combinacion[0]])
-        equipo2 = ", ".join([n[0] for n in mejor_combinacion[1]])
-        equipo3 = ", ".join([n[0] for n in mejor_combinacion[2]])
-        writer.writerow([idx+1, equipo1, equipo2, equipo3, max_puntuacion])
 
 # Escritura de la tabla de resultados para la segunda parte del problema en un archivo de salida diferente
-with open(args.archivo_salida, 'a',encoding='utf-8', newline='') as archivo:
+with open(args.archivo_salida, 'w',encoding='utf-8', newline='') as archivo:
     writer = csv.writer(archivo)
-    writer.writerow('\n')
     writer.writerow(["Combinación", "Equipo 1", "Puntuación media"])
     for idx, mejor_combinacion in enumerate(mejores_combinaciones_medias):
         equipo1 = ", ".join([n[0] for n in mejor_combinacion[0]])
         writer.writerow([idx+1, equipo1, mejor_combinacion[1]])
+    
